7f8c01bc48266ed2a9edfe909578c74d1f0b14a6
Steven Pousty <spousty@redhat.com>
2016-02-15 13:03:14 -0800

    adding more of my steps so I can ditch evernote

--- utils/pre-package.sh
+++ utils/pre-package.sh
@@ -3,6 +3,8 @@
 #
 # Preparing the box for packaging. Will remove all unneeded logs, etc...
 
+# make sure you you have already added sample-app from the origin repo to the installation
+# https://github.com/openshift/origin/blob/master/examples/sample-app/application-template-stibuild.json
 
 # Remove Non used containers
 _exited=$(docker ps -aqf "status=exited")
@@ -16,6 +18,21 @@ _untagged=$(docker images | grep "<none>" | awk '{print $3}')
 [ "" != "${_untagged}" ] && echo "[INFO] Deleting untagged images" && docker rmi ${_untagged}
 _dangling=$(docker images -f "dangling=true" -q)
 [ "" != "${_dangling}" ] && echo "[INFO] Deleting dangling images" && docker rmi ${_dangling}
+
+curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > .ssh/authorized_keys 
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
+chown -R vagrant:vagrant .ssh
+oc edit scc restricted #change RunAsUser to RunAsAny
+
+#for Postgres from Crunchy to work
+#in /etc/passwd:
+#postgres:x:26:26:PostgreSQL Server:/var/lib/pgsql:/bin/bash
+
+#in /etc/group:
+#postgres:x:26:
+
+
 
 # Stop services
 echo "[INFO] Stopping Origin service"
@@ -45,3 +62,7 @@ echo "[INFO] Compacting disk"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 sync
+
+#final step
+# vagrant package --base origin --output openshift3-1.1.1.1.box --vagrantfile ~/openshiftVagrant/Vagrantfile
+# then upload it to Atlas
