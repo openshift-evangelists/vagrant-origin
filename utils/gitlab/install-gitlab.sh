36c6901068b4480a43ed40dbcbfbe7e9b3ee4256
Jorge Morales <jorgemoralespou@gmail.com>
2016-06-07 23:01:41 +0200

    Fixed script

--- utils/gitlab/install-gitlab.sh
+++ utils/gitlab/install-gitlab.sh
@@ -28,11 +28,11 @@ sudo_oc create -f /utils/gitlab/gitlab-template.json -n gitlab
 sudo_oc create -f /utils/gitlab/gitlab-pvcs.json -n gitlab
 
 # Create nfs with no_root_squash
-/utils/gitlab/create-nfs-pv.sh pv-gitlab-redis-data gitlab gitlab-ce-redis-data
-/utils/gitlab/create-nfs-pv.sh pv-gitlab-etc        gitlab gitlab-ce-etc
-/utils/gitlab/create-nfs-pv.sh pv-gitlab-logs       gitlab gitlab-ce-logs
-/utils/gitlab/create-nfs-pv.sh pv-gitlab-opt        gitlab gitlab-ce-opt
-/utils/gitlab/create-nfs-pv.sh pv-gitlab-postgresql gitlab gitlab-ce-postgresql
+/utils/gitlab/create-nfs-pv.sh pv-gitlab-redis-data gitlab gitlab-redis-data
+/utils/gitlab/create-nfs-pv.sh pv-gitlab-etc        gitlab gitlab-etc
+/utils/gitlab/create-nfs-pv.sh pv-gitlab-logs       gitlab gitlab-logs
+/utils/gitlab/create-nfs-pv.sh pv-gitlab-opt        gitlab gitlab-opt
+/utils/gitlab/create-nfs-pv.sh pv-gitlab-postgresql gitlab gitlab-postgresql
 
 # Create the application
 sudo_oc new-app gitlab-ce -n gitlab
